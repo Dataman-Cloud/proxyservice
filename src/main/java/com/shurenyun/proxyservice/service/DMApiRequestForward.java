f93600757c1455751809d733981a0408d5a6fb83
yqguodataman <yqguo@dataman-inc.com>
2016-09-21 13:57:18 +0800

    update

--- src/main/java/com/shurenyun/proxyservice/service/DMApiRequestForward.java
+++ src/main/java/com/shurenyun/proxyservice/service/DMApiRequestForward.java
@@ -2,6 +2,7 @@ package com.shurenyun.proxyservice.service;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
@@ -16,10 +17,17 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.shurenyun.proxyservice.entity.ServiceResponse;
+import com.shurenyun.proxyservice.entity.StackResponse;
 import com.shurenyun.proxyservice.util.Properties;
 
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 @Service
 public class DMApiRequestForward {
@@ -85,19 +93,94 @@ public class DMApiRequestForward {
 	 */
 	public String searchStack(String stack_name) {
 		
+		String jsonInString = "";
+		
 		RestTemplate searchStackRestTemplate = new RestTemplate();
 		searchStackRestTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 		searchStackRestTemplate.getMessageConverters().add(new StringHttpMessageConverter());
         
-		String uri = new String(this.configuration.getApi()+"/api/v1/stacks/"+stack_name);
-		
+		String uri = new String(this.configuration.getApi()+"/api/v1/stacks/"+stack_name+"/services");
 		HttpEntity<String> request = new HttpEntity<String>("");
 		ResponseEntity<String> responseEntity = searchStackRestTemplate.exchange(uri, HttpMethod.GET, request, String.class);
 		
-		log.debug(responseEntity.getBody().toString());
+		JSONParser parser = new JSONParser();
+		StackResponse stackResponse = new StackResponse();
 		
-		return responseEntity.getBody().toString();
+		try {
+			JSONObject stack = (JSONObject) parser.parse(responseEntity.getBody().toString());
+			JSONArray services = (JSONArray) parser.parse(stack.get("data").toString());
+			List<ServiceResponse> list  = new ArrayList<ServiceResponse>();
+			for (int i = 0;i < services.size(); i++) {
+				String managerUrl = "";
+				String nodeuri = new String(this.configuration.getApi()+"/api/v1/nodes");
+				ResponseEntity<String> nodesResponse = searchStackRestTemplate.exchange(nodeuri, HttpMethod.GET, new HttpEntity<String>(""), String.class);
+				JSONObject json = (JSONObject) JSONValue.parse(nodesResponse.getBody().toString());
+				JSONArray nodes = (JSONArray) JSONValue.parse(json.get("data").toString());
+				for (int y = 0; y < nodes.size(); y++) {
+					JSONObject node = (JSONObject) JSONValue.parse(nodes.get(y).toString());
+					JSONObject isManager = (JSONObject) JSONValue.parse(node.get("ManagerStatus").toString());
+					
+					if (Boolean.parseBoolean(isManager.get("Leader").toString()) && 
+							isManager.get("Reachability").toString().equals("reachable")) {
+						JSONObject labels = (JSONObject) ((JSONObject)JSONValue.parse(node.get("Spec").toString())).get("Labels");
+						String[] url = labels.get("dm.reserved.node.endpoint").toString().split(":");
+						if (url.length == 3) {
+							managerUrl = url[1];
+						} else {
+							managerUrl = url[0];
+						}
+						break;
+					}
+				}
+
+				ServiceResponse sr = new ServiceResponse();
+				JSONObject service = (JSONObject) parser.parse(services.get(i).toString());
+				int numTasksTotal = Integer.parseInt(service.get("NumTasksTotal").toString());
+				int numTasksRunning = Integer.parseInt(service.get("NumTasksRunning").toString());
+				if (numTasksTotal == 0 && numTasksRunning == 0) {
+					sr.setStatus("not stated");
+				} else if (numTasksRunning == numTasksTotal) {
+					sr.setStatus("running");
+				} else {
+					sr.setStatus("");
+				}
+				
+				sr.setId(service.get("ID").toString());
+				sr.setName(service.get("Name").toString());
+				
+				String serviceuri = new String(this.configuration.getApi()+"/api/v1/stacks/"+stack_name+"/services/" + sr.getName());
+				ResponseEntity<String> response = searchStackRestTemplate.exchange(serviceuri, HttpMethod.GET, new HttpEntity<String>(""), String.class);
+				JSONObject sinfo = (JSONObject)JSONValue.parse(response.getBody().toString());
+				JSONObject data = (JSONObject)JSONValue.parse(sinfo.get("data").toString());
+				JSONObject endpoint = (JSONObject) JSONValue.parse(data.get("Endpoint").toString());
+				JSONArray ports = (JSONArray) JSONValue.parse(endpoint.get("Ports").toString());
+				for (int x = 0;x < ports.size(); x++) {
+					JSONObject port = (JSONObject) JSONValue.parse(ports.get(x).toString());
+					sr.setUrl("http://" +managerUrl+":" + port.get("PublishedPort").toString());
+				}
+				
+				
+				list.add(sr);
+			}
+			stackResponse.setApp_list(list);
+			ObjectMapper mapper = new ObjectMapper();
+			jsonInString = mapper.writeValueAsString(stackResponse);
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+
+		}
+		
+		//log.debug(responseEntity.getBody().toString());
+		//return responseEntity.getBody().toString();
+		
+		//log.debug(jsonInString);
 	
+		log.debug(jsonInString);
+		return jsonInString;
 	}
 	
 	/**
